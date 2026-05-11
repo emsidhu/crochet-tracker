789de7c68bea6ed097fd374ae2bfbee960a9bbea
emsidhu <68081202+emsidhu@users.noreply.github.com>
2026-05-11 01:20:52 -0700

    Initialize Spring

---
+++ src/main/java/com/github/emsidhu/crochet_tracker/CrochetTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.github.emsidhu.crochet_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrochetTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrochetTrackerApplication.class, args);
+	}
+
+}
